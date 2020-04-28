7cca8ae0fca1bf813d7c33265ca4b8d5e53830d1
Dricosta <adrianoc55@hotmail.com>
2020-04-28 19:41:58 -0300

    config banco de dados heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'docker',
-    database: 'db_seguranca',
+    host: 'ec2-34-234-228-127.compute-1.amazonaws.com',
+    username: 'arfzjcupsmjqaa',
+    password: '9d5f4d5927a22270d5f5b7d6df528d60236ebb0b3cbd3913e16dd73df8842394',
+    database: 'de7t13671ahusq',
     define: {
         timestamps: true,
         underscored: true,
